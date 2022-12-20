c7e7676623adaa7423a234a1b4cdb4c1ed0732e4
Tisham Dhar <whatnickd@gmail.com>
2022-12-21 10:06:02 +1030

    Start refactoring for full run
    Signed-off-by: Tisham Dhar <whatnickd@gmail.com>

--- README.md
+++ README.md
@@ -16,4 +16,12 @@ Designed to download all car safety tests data from ANCAP website for analysis
 ## Roadmap
 - Set up [template matching](https://github.com/whatnick/ancap_scraper/issues/1) to fine align area of image from which to extract table
 - Use [table detection](https://github.com/asagar60/TableNet-pytorch) image analysis methods to [auto-detect location of tables](https://github.com/whatnick/ancap_scraper/issues/2) in images
-- [Retrain SVM](https://github.com/whatnick/ancap_scraper/issues/3) using randomly placed templates
+  - Table Detection Code samples from Hugging Face : [Microsoft](https://huggingface.co/microsoft/table-transformer-detection), [TahaDouaji](https://huggingface.co/TahaDouaji/detr-doc-table-detection)
+- [Retrain SVM](https://github.com/whatnick/ancap_scraper/issues/3) using randomly placed templates.
+- Retrain Table detection and column detection using ANCAP specific dataset.
+- Stitch all of the steps of the code together to run on schedule and save all results in S3 (PDF's from ANCAP) + DynamoDB (Safety Features).
+- Add manual canonical safety features to error-correct OCR if that happens.
+- Try Random forest and Neural Network based symbology classifiers.
+- Add a web service to lookup vehicle models by particular set of safety features.
+- Link to vehicle images.
+- Extract test result photos for impact tests on bonnet etc.
--- ancap_scraper/detect_table.py
+++ ancap_scraper/detect_table.py
@@ -3,27 +3,57 @@
 import sys
 
 from transformers import DetrImageProcessor, DetrForObjectDetection
+from transformers import AutoFeatureExtractor, AutoModelForObjectDetection
+
 import torch
 from PIL import Image
 
-url = sys.argv[1]
-image = Image.open(url)
+def main():
+        url = sys.argv[1]
+        image = Image.open(url)
 
-processor = DetrImageProcessor.from_pretrained("table-detect/taha")
-model = DetrForObjectDetection.from_pretrained("table-detect/taha")
+        extract_taha(image)
+        extract_ms(image)
 
-inputs = processor(images=image, return_tensors="pt")
-outputs = model(**inputs)
+def extract_taha(image : Image):
+        processor = DetrImageProcessor.from_pretrained("table-detect/taha")
+        model = DetrForObjectDetection.from_pretrained("table-detect/taha")
 
-# convert outputs (bounding boxes and class logits) to COCO API
-# let's only keep detections with score > 0.9
-target_sizes = torch.tensor([image.size[::-1]])
-results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
-print(results)
+        inputs = processor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
 
-for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
-    box = [round(i, 2) for i in box.tolist()]
-    print(
-            f"Detected {model.config.id2label[label.item()]} with confidence "
-            f"{round(score.item(), 3)} at location {box}"
-    )
+        # convert outputs (bounding boxes and class logits) to COCO API
+        # let's only keep detections with score > 0.9
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+        print(results)
+
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+                box = [round(i, 2) for i in box.tolist()]
+                print(
+                        f"Detected {model.config.id2label[label.item()]} with confidence "
+                        f"{round(score.item(), 3)} at location {box}"
+                )
+
+def extract_ms(image : Image):
+        extractor = AutoFeatureExtractor.from_pretrained("table-detect/microsoft")
+        model = AutoModelForObjectDetection.from_pretrained("table-detect/microsoft")
+
+        inputs = extractor(images=image, return_tensors="pt")
+        outputs = model(**inputs)
+
+        # convert outputs (bounding boxes and class logits) to COCO API
+        # let's only keep detections with score > 0.9
+        target_sizes = torch.tensor([image.size[::-1]])
+        results = extractor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+        print(results)
+
+        for score, label, box in zip(results["scores"], results["labels"], results["boxes"]):
+                box = [round(i, 2) for i in box.tolist()]
+                print(
+                        f"Detected {model.config.id2label[label.item()]} with confidence "
+                        f"{round(score.item(), 3)} at location {box}"
+                )
+
+if __name__ == "__main__":
+        main()
